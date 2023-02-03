de355653660af85f78c84de2cdaf0fff9b6a4135
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-02-03 12:18:02 -0500

    added tapping conditions and worked on Mana cost

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,16 +1,32 @@
 class Character {
-    constructor(name, type, subtype, attack, defend, manaValue, color) {
+    constructor(name, type, subtype, attack, defend, colorCost, colorlessCost, color) {
         this.name = name;
         this.type = type;
         this.subtype = subtype;
         this.attack = attack;
         this.defend = defend;
-        this.manaValue = manaValue;
+        this.colorCost = colorCost;
+        this.colorlessCost = colorlessCost
         this.color = color;
     }
     printStats() {
-        console.log(`Name: ${this.name}, Type: ${this.type}, Subtype: ${this.subtype}, Attack: ${this.attack}, Defend: ${this.defend}, Mana Cost: ${this.manaValue}, Color: ${this.color}`)
+        console.log(`Name: ${this.name}, Type: ${this.type}, Subtype: ${this.subtype}, Attack: ${this.attack}, Defend: ${this.defend}, Color Cost: ${this.colorCost}, Colorless Cost: ${this.colorlessCost}, Color: ${this.color}`)
     }
+    castSpell(manaPool) {
+        let onTable = false;
+        if (this.color === colorless) {
+            if (this.colorlessCost < manaPool.totalMana) {
+                onTable = true;
+                manaPool.totalMana = manaPool.totalMana - this.colorlessCost;
+            } else {
+                return "You need more mana to play this!"
+            }
+        } else {
+            return "You need more mana to play this!"
+
+        }
+    }
+
     attack(opponent) {
         opponent.defend = opponent.defend - this.attack;
     }
@@ -21,56 +37,80 @@ class Character {
             return true
         }
     }
+
 }
 
-const meteorGolem = new Character("Meteor Golem", "artifact creature", "golem", 3, 3, 7, "colorless");
+const meteorGolem = new Character("Meteor Golem", "artifact creature", "golem", 3, 3, 0, 7, "colorless");
 meteorGolem.printStats();
 
 class Land {
-    constructor(type, name, color) {
+    constructor(type, name, color, tapped) {
         this.type = type;
         this.name = name;
         this.color = color;
+        this.tapped = tapped;
     }
     printStats() {
-        console.log(`Type: ${this.type}, Name: ${this.name}, Color: ${this.color}`)
+        console.log(`LAND::: Type: ${this.type}, Name: ${this.name}, Color: ${this.color}, Tapped: ${this.tapped}`)
     }
     tapForMana(manaPool) {
-        manaPool.addMana(this.color)
+        if (this.tapped === false) {
+            manaPool.addMana(this.color)
+            this.tapped = true;
+        } else {
+            return "This land has already been tapped!"
+        }
+
     }
 }
 
-const forest = new Land("basic", "forest", "green");
+const forest = new Land("basic", "forest", "green", false);
+const mountain = new Land("basic", "mountain", "red", false);
+const swamp = new Land("basic", "swamp", "black", false);
+const plains = new Land("basic", "plains", "white", false);
+const island = new Land("basic", "island", "blue", false);
 forest.printStats()
 
 class ManaPool {
-    constructor(white, blue, black, red, green) {
+    constructor(white, blue, black, red, green, colorless, totalMana) {
         this.white = white;
         this.blue = blue;
         this.black = black;
         this.red = red;
         this.green = green;
+        this.colorless = colorless;
+        this.totalMana = totalMana;
     }
     addMana(color) {
         switch (color) {
             case "white":
                 this.white++
+                this.totalMana++
                 break
             case "blue":
                 this.blue++
+                this.totalMana++
                 break
             case "black":
                 this.black++
+                this.totalMana++
                 break
             case "red":
                 this.red++
+                this.totalMana++
                 break
             case "green":
                 this.green++
+                this.totalMana++
+                break
+            case "colorless":
+                this.colorless++
+                this.totalMana++
                 break
         }
     }
+
 }
 
-const myMana = new ManaPool(0, 0, 0, 0, 0)
+const myMana = new ManaPool(0, 0, 0, 0, 0, 0)
 console.log(myMana)
