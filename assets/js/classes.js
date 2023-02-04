be4e3a8e295d345603965beac4d04eac8dc2247f
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-02-04 00:25:35 -0500

    added deck file and created green deck, added instant, sorcery, planeswalker classes

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -14,6 +14,7 @@ class Character {
     }
     castSpell(manaPool) {
         let onTable = false;
+
         if (this.color === colorless) {
             if (this.colorlessCost < manaPool.totalMana) {
                 onTable = true;
@@ -41,7 +42,59 @@ class Character {
 }
 
 const meteorGolem = new Character("Meteor Golem", "artifact creature", "golem", 3, 3, 0, 7, "colorless");
+const bristlingBoar = new Character("Bristling Boar", "creature", "boar", 4, 3, 1, 3, "green");
 meteorGolem.printStats();
+bristlingBoar.printStats();
+
+class Enchantment {
+    constructor(name, type, subtype, colorCost, colorlessCost, text, color) {
+        this.name = name;
+        this.type = type;
+        this.subtype = subtype;
+        this.colorCost = colorCost;
+        this.colorlessCost = colorlessCost;
+        this.text = text;
+    }
+}
+
+class Sorcery {
+    constructor(name, type, subtype, colorCost, colorlessCost, text, color) {
+        this.name = name;
+        this.type = type;
+        this.subtype = subtype;
+        this.colorCost = colorCost;
+        this.colorlessCost = colorlessCost;
+        this.text = text
+    }
+}
+
+class Instant {
+    constructor(name, type, subtype, colorCost, colorlessCost, text, color) {
+        this.name = name;
+        this.type = type;
+        this.subtype = subtype;
+        this.colorCost = colorCost;
+        this.colorlessCost = colorlessCost;
+        this.text = text;
+        this.color = color;
+    }
+}
+
+class Planeswalker {
+    constructor(name, type, subtype, loyalty1, loyalty1Text, loyalty2, loyalty2Text, loyalty3, loyalty3Text, startingLoyalty, color) {
+        this.name = name;
+        this.type = type;
+        this.subtype = subtype;
+        this.loyalty1 = loyalty1;
+        this.loyalty1Text = loyalty1Text;
+        this.loyalty2 = loyalty2;
+        this.loyalty2Text = loyalty2Text;
+        this.loyalty3 = loyalty3;
+        this.loyalty3Text = loyalty3Text;
+        this.startingLoyalty = startingLoyalty;
+        this.color = color;
+    }
+}
 
 class Land {
     constructor(type, name, color, tapped) {
@@ -114,3 +167,11 @@ class ManaPool {
 
 const myMana = new ManaPool(0, 0, 0, 0, 0, 0)
 console.log(myMana)
+
+exports.Character = Character;
+exports.Land = Land;
+exports.ManaPool = ManaPool;
+exports.Enchantment = Enchantment;
+exports.Sorcery = Sorcery;
+exports.Instant = Instant;
+exports.Planeswalker = Planeswalker;
